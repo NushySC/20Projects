6ed029c768717c1b61eb5fd7f0af9ef03a7532ac
Anuska Sampedro <asampedro@vectoritcgroup.com>
2020-08-17 19:49:15 +0200

    wip: store

--- 2. Movie/index.js
+++ 2. Movie/index.js
@@ -5,19 +5,36 @@ const total = document.getElementById('total');
 
 const movieSelect = document.getElementById('movie');
 
+// populateUI = () => {
+// 	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+// 	console.log(selectedSeats);
+// };
+// populateUI();
 movieSelect.addEventListener('change', (e) => {
 	ticketPrice = +e.target.value;
 	updatedSelectedCount();
 });
 
 let ticketPrice = movieSelect.value;
+setMovieData = (movieIndex, moviePrice) => {
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 updatedSelectedCount = () => {
 	const selectedSeats = document.querySelectorAll('.seat.selected');
-	console.log(movieSelect.value);
-	// console.log(selectedSeats);
-	const selectedSeatsCount = selectedSeats.length;
-	count.innerText = selectedSeatsCount;
-	total.innerText = `$ ${selectedSeatsCount * ticketPrice}`;
+	// console.log(movieSelect.value);
+	//console.log(selectedSeats);
+
+	const seatsIndex = [...selectedSeats].map((seat) =>
+		[...seats].indexOf(seat)
+	);
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+	// console.log(seatsIndex);
+	const selectedSeatsCount = selectedSeats.length - 1;
+	count.innerText = `${selectedSeatsCount} seats`;
+	total.innerText = `$${selectedSeatsCount * ticketPrice}`;
 };
 
 container.addEventListener('click', (e) => {
